a129402666a783c2acbe117484ab915d3aecaa1f
Lucio Benfante <lucio@benfante.com>
2023-04-21 15:42:09 +0200

    Added MVC controller example

---
+++ first-example/src/main/java/com/benfante/teaching/springboot/firstexample/controllers/mvc/HomeController.java
@@ -0,0 +1,14 @@
+package com.benfante.teaching.springboot.firstexample.controllers.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+
+}
