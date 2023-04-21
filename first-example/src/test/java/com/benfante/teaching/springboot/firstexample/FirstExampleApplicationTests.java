98df37507a6b885d59ff3ad8eb4d47c567ded9a2
Lucio Benfante <lucio@benfante.com>
2023-04-21 15:13:20 +0200

    First example initial commit

---
+++ first-example/src/test/java/com/benfante/teaching/springboot/firstexample/FirstExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benfante.teaching.springboot.firstexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
