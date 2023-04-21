cc987401a33772a30e514628e07fb6e4722e6be2
Lucio Benfante <lucio@benfante.com>
2023-04-21 16:20:55 +0200

    Added REST Controller example

---
+++ first-example/src/main/java/com/benfante/teaching/springboot/firstexample/controllers/rest/PersonController.java
@@ -0,0 +1,31 @@
+package com.benfante.teaching.springboot.firstexample.controllers.rest;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.benfante.teaching.springboot.firstexample.model.Person;
+import com.benfante.teaching.springboot.firstexample.services.PersonService;
+
+@RestController
+@RequestMapping("/api/v1/people")
+public class PersonController {
+    
+    @Autowired
+    private PersonService personService;
+
+    @GetMapping()
+    private List<Person> getAll() {
+        return personService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    private Optional<Person> getById(@PathVariable Long id) {
+        return personService.findById(id);
+    }
+}
---
+++ first-example/src/main/java/com/benfante/teaching/springboot/firstexample/services/PersonService.java
@@ -0,0 +1,27 @@
+package com.benfante.teaching.springboot.firstexample.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.benfante.teaching.springboot.firstexample.model.Person;
+
+@Service
+public class PersonService {
+    
+    private List<Person> data = List.of(
+        new Person(1L, "Mario", "Rossi"),
+        new Person(2L, "Giovanna", "Bianchi"),
+        new Person(3L, "Carlo", "Neri"),
+        new Person(1L, "Laura", "Biachi")
+    );
+
+    public List<Person> getAll() {
+        return this.data;
+    }
+
+    public Optional<Person> findById(Long id) {
+        return this.data.stream().filter(person -> person.getId().equals(id)).findFirst();
+    }
+}
