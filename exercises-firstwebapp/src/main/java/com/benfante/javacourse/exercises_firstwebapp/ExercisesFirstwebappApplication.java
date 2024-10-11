714b9d9fa2c84eb923d598b459a355d7817c058e
Lucio Benfante <lucio@benfante.com>
2024-10-11 11:02:11 +0200

    Added initial first webapp

---
+++ exercises-firstwebapp/src/main/java/com/benfante/javacourse/exercises_firstwebapp/ExercisesFirstwebappApplication.java
@@ -0,0 +1,13 @@
+package com.benfante.javacourse.exercises_firstwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercisesFirstwebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercisesFirstwebappApplication.class, args);
+	}
+
+}
---
+++ exercises-firstwebapp/src/test/java/com/benfante/javacourse/exercises_firstwebapp/ExercisesFirstwebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benfante.javacourse.exercises_firstwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExercisesFirstwebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
