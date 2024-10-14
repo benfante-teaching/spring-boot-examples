6319622a2a1b7bf9773db857dc8e6c23d53bd4d7
Lucio Benfante <lucio@benfante.com>
2024-10-14 05:19:55 +0200

    Added initial people-rest project

---
+++ people-rest/src/main/java/com/benfante/javacourse/people_rest/PeopleRestApplication.java
@@ -0,0 +1,13 @@
+package com.benfante.javacourse.people_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeopleRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeopleRestApplication.class, args);
+	}
+
+}
