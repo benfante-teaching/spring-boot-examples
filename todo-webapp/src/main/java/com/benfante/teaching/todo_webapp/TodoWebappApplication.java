20e0889c2b12af9f30edec587279bf9dc4b689a7
Lucio Benfante <lucio@benfante.com>
2025-04-06 10:21:26 +0200

    feat: Initialize Spring Boot Todo Web Application with Maven Wrapper and basic structure

---
+++ todo-webapp/src/main/java/com/benfante/teaching/todo_webapp/TodoWebappApplication.java
@@ -0,0 +1,13 @@
+package com.benfante.teaching.todo_webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoWebappApplication.class, args);
+	}
+
+}
---
+++ todo-webapp/src/test/java/com/benfante/teaching/todo_webapp/TodoWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benfante.teaching.todo_webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
